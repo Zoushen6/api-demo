f0b4685fe0c6ad25395ae7d7772b19afe3865739
Zoushen6 <1131717009@qq.com>
2022-03-11 15:05:49 +0800

    proxy and api

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/************AXIOS**************/
+import http from "./tools/promise.js"; //请求封装
+Vue.prototype.$http = http;
+/************AXIOS**************/
+
 Vue.config.productionTip = false
 
 new Vue({
